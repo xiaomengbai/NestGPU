c4fbf506fab835d8a48b07e205800656404bc270
Sofoklis Floratos <floratos.1@osu.edu>
2019-09-04 16:54:57 -0400

    Add TPC-H Q4

---
+++ test/tpch_test/q4.sql
@@ -0,0 +1,22 @@
+--TPC-H Q4 (Type J)
+select
+   o_orderpriority,
+   count(*) as order_count 
+from
+   orders 
+where
+--   o_orderdate >= date '[DATE]'
+--   and o_orderdate < date '[DATE]' + interval '3' month 
+   and exists (
+       select 
+             *
+       from 
+             lineitem
+       where
+             l_orderkey = o_orderkey
+             and l_commitdate < l_receiptdate
+) 
+group by
+   o_orderpriority 
+order by
+   o_orderpriority;
