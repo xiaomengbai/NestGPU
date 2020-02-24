3b5683b4bbb764654ea9d261288d8a9103a35a9c
Sofoklis Floratos <floratos.1@osu.edu>
2020-02-24 11:25:08 -0500

    Scripts for CPU vs GPU on IR2

--- test/tpch_psql/deployment/initdb.sh
+++ test/tpch_psql/deployment/initdb.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 #Postgresql dir
-PSQL_DIR=/home/sofoklis/workspace/subquerygpu2/psql
+PSQL_DIR=/home/floratos.1/workspace/subquery/test/tpch_psql/postgresql-12.1
 
 #Delete previous data folder
-rm -r $PSQL_DIR/data-postgresql-11.5/
-mkdir $PSQL_DIR/data-postgresql-11.5/
+rm -r $PSQL_DIR/data
+mkdir $PSQL_DIR/data
 
 #Init directory
-$PSQL_DIR/bin-postgresql-11.5/bin/initdb -D $PSQL_DIR/data-postgresql-11.5/
+$PSQL_DIR/bin/initdb -D $PSQL_DIR/data
 
 #Start db
-$PSQL_DIR/bin-postgresql-11.5/bin/pg_ctl -D $PSQL_DIR/data-postgresql-11.5/ -l $PSQL_DIR/data-postgresql-11.5/logfile start
+$PSQL_DIR/bin/pg_ctl -D $PSQL_DIR/data -l $PSQL_DIR/data/logfile start
 
 #Init database
-$PSQL_DIR/bin-postgresql-11.5/bin/createdb testdb
+$PSQL_DIR/bin/createdb testdb
 
 #Stop db
-$PSQL_DIR/bin-postgresql-11.5/bin/pg_ctl -D $PSQL_DIR/data-postgresql-11.5/ -l $PSQL_DIR/data-postgresql-11.5/logfile stop
+$PSQL_DIR/bin/pg_ctl -D $PSQL_DIR/data -l $PSQL_DIR/data/logfile stop
--- test/tpch_psql/deployment/stopdb.sh
+++ test/tpch_psql/deployment/stopdb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
   
 #Postgresql dir
-PSQL_DIR=/home/sofoklis/workspace/subquerygpu2/psql
+PSQL_DIR=/home/floratos.1/workspace/subquery/test/tpch_psql/postgresql-12.1
 
-$PSQL_DIR/bin-postgresql-11.5/bin/pg_ctl -D $PSQL_DIR/data-postgresql-11.5/ -l $PSQL_DIR/data-postgresql-11.5/logfile stop
+$PSQL_DIR/bin/pg_ctl -D $PSQL_DIR/data -l $PSQL_DIR/data/logfile stop
