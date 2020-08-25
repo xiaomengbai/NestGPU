e8b1da5e3b2dbab8ff03c60f23864de9f70c0ba6
Mengbai Xiao <xiaomengbai@gmail.com>
2020-08-25 03:47:58 -0400

    change q2_unnested to the original form

--- test/tpch_test/q2_unnested.sql
+++ test/tpch_test/q2_unnested.sql
@@ -34,16 +34,16 @@ select
    and s_suppkey = ps_suppkey
    and p_size = 20
    and p_type like 'MEDIUM%'
-   and p_brand = 'Brand#41'
+   --and p_brand = 'Brand#41'
    --and p_brand like 'Brand#4%'
    and s_nationkey = n_nationkey
    and n_regionkey = r_regionkey
-   --and r_name = 'ASIA'
+   and r_name = 'ASIA'
    and ps_supplycost = t1_min_supplycost
    and p_partkey = t1_partkey
---  order by
---    s_acctbal,
---    n_name,
---    s_name,
---    p_partkey
+order by
+   s_acctbal,
+   n_name,
+   s_name,
+   p_partkey
 ;
